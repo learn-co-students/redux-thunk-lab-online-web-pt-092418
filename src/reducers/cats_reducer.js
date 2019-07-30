77eb948ac972c00cd4202113e8edc61dabaf8a79
Virginia Dooley <vcdooley@gmail.com>
2019-07-30 08:19:21 -0700

    remove comments

--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -1,7 +1,4 @@
-// Define your catsReducer in src/reducers/cats_reducer.
-// Your catsReducer should respond to two actions with types of
-// 'LOADING_CATS' and a 'FETCH_CATS' respectively.
-///reducer///
+
   const initialState = {
     loading: false,
     pictures: []
@@ -17,7 +14,6 @@ export default (state = initialState, action) => {
 
     case 'FETCH_CATS': {
       return {...state, loading: false, pictures: action.payload};
-      // return state.concat(action.images);
     }
 
     default:
