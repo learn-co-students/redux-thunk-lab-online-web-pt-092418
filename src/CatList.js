c0e35207a320b62f4c4e6e8618330a2a31ee9c5e
github username <github email address>
2019-07-29 21:14:53 -0400

    add some testing codes and comment them out

--- src/CatList.js
+++ src/CatList.js
@@ -9,3 +9,16 @@ class CatList extends Component {
   }
 }
 export default CatList
+
+// //Hacker news testing: 
+// import React, { Component } from 'react'
+//
+// class CatList extends Component {
+//   render () {
+//     const list = this.props.catPics.map((cat, index) => <img  key={index} src={cat.url} height="20%" width="20%" alt="catImage" /> )
+//     return (
+//       <div>{list}</div>
+//     )
+//   }
+// }
+// export default CatList
