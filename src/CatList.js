28f706d4f0fecdc4ca3d91a4f2c2bf63b3d1dd80
github username <github email address>
2019-07-29 21:05:34 -0400

    clean up comments

--- src/CatList.js
+++ src/CatList.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 
 class CatList extends Component {
-
   render () {
-    // debugger
     const list = this.props.catPics.map((cat, index) => <img  key={index} src={cat.url} height="20%" width="20%" alt="catImage" /> )
     return (
       <div>{list}</div>
     )
   }
-
 }
-
 export default CatList
